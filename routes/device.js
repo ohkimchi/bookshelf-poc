6f54ac5ee4120d3c7e780325598eca34a3c29fe1
Max Xu <max.xu@biqmind.com>
2019-05-30 17:41:18 +0800

    router done, need test

--- routes/device.js
+++ routes/device.js
@@ -1,6 +1,6 @@
-import * as express from 'express'
-import { Device } from '../models/Device'
-import { Person } from '../models/Person'
+import * as express from 'express';
+import { Device } from '../models/Device';
+import { Person } from '../models/Person';
 
 var device = express.Router();
 
@@ -10,25 +10,92 @@ device.route('/devices')
       .where(req.query)
       .fetchAll()
       .then((devices) => {
-        res.json({ devices });
+        res.json(devices);
       });
   });
 
 device.route('/devices/registered')
-.get()
+  .get((req, res) => {
+    Device
+      .where('registered', true)
+      .fetch()
+      .then((device) => {
+        res.json(device);
+      });
+  });
 
 device.route('/devices/:deviceId')
-.get()
+  .get((req, res) => {
+    Device
+      .where('deviceId', req.params.deviceId)
+      .fetch()
+      .then((device) => {
+        res.json(device);
+      });
+  });
 
 device.route('/devices/person/:personId')
-.get()
+  .get((req, res) => {
+    Device
+      .where('owned_by', req.params.personId)
+      .fetch()
+      .then((device) => {
+        res.json(device);
+      });
+  });
 
 device.route('/devices/register')
-.post()
+  .post((req, res) => {
+    new Device({
+      type: req.body.type,
+      location: req.body.location,
+      macaddr: req.body.macaddr,
+      registered: req.body.registered,
+      owned_by: req.body.personId,
+    })
+      .save()
+      .then((saved) => {
+        res.json({ saved });
+      });
+  });
 
 device.route('/devices/person')
-.post()
+  .post((req, res) => {
+    Device
+      .where('deviceId', req.body.deviceId)
+      .fetch()
+      .then((device) => {
+        device
+          .save({
+            type: req.body.type,
+            location: req.body.location,
+            macaddr: req.body.macaddr,
+            registered: req.body.registered,
+            owned_by: req.body.personId,
+          })
+          .then((saved) => {
+            res.json({ saved });
+          });
+      });
+  });
 
 device.route('/device/:deviceId')
-.put()
+  .put((req, res) => {
+    Device
+      .where('deviceId', req.body.deviceId)
+      .fetch()
+      .then((device) => {
+        device
+          .save({
+            type: req.body.type,
+            location: req.body.location,
+            macaddr: req.body.macaddr,
+            registered: req.body.registered,
+            owned_by: req.body.personId,
+          })
+          .then((saved) => {
+            res.json({ saved });
+          });
+      });
+  });
 
